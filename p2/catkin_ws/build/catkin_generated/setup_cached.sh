7447059f0e98382a061b522b81a85086e43b35ae
root <zheondre@gmail.com>
2020-05-15 08:15:47 +0000

    setting up 2nd project

---
+++ p2/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/workspace/Ros_Gazebo/p2/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/workspace/Ros_Gazebo/p2/catkin_ws/build"
+export PYTHONPATH="/home/workspace/Ros_Gazebo/l3/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/workspace/Ros_Gazebo/p2/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/workspace/Ros_Gazebo/p2/catkin_ws/src:$ROS_PACKAGE_PATH"
