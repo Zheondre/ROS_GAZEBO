d05b2f37f4e46a1ce6f544932333e94df6f2d406
root <zheondre@gmail.com>
2020-05-18 09:33:42 +0000

    saving progress

--- p2/catkin_ws/build/catkin_generated/order_packages.py
+++ p2/catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/workspace/Ros_Gazebo/p2/catkin_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/workspace/Ros_Gazebo/l3/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/workspace/Ros_Gazebo/l3/catkin_ws/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
